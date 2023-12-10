420658afe6bef9364cab6df599001cf9fc5b99f4
3dida <44850191+aallali@users.noreply.github.com>
2023-12-10 11:43:25 +0100

    [+] : more projects

--- components/Projects.vue
+++ components/Projects.vue
@@ -11,17 +11,18 @@
                 description="An engine designed play Gomoku and win everytime" :isCover="true" />
 
             <ProjectCard title="Leaffliction"
-                image="https://media.springernature.com/lw685/springer-static/image/art%3A10.1038%2Fs41598-022-21498-5/MediaObjects/41598_2022_21498_Fig1_HTML.png?as=webp"
-                description="Detection and Classification of Plant Disease with Deep Learning" />
+                image="https://camo.githubusercontent.com/4a40452bf14786ccfb502f5524574fb593536bff9d06bd3c0a5c14d232646131/68747470733a2f2f692e696d6775722e636f6d2f59734d697936422e676966"
+                description="Here, I perform the analysis, augmentation and transformation of a leaf dataset. After image classification using a Convolutional Neural Network."
+                :isCover="true" />
+            <!-- image="https://media.springernature.com/lw685/springer-static/image/art%3A10.1038%2Fs41598-022-21498-5/MediaObjects/41598_2022_21498_Fig1_HTML.png?as=webp" -->
 
             <ProjectCard title="Linear Regression from scratch"
                 image="https://raw.githubusercontent.com/aallali/ft-linear-regression/main/pic/LR-Live.gif"
                 description="The aim of this project is to introduce you to the basic concept behind machine learning. For this project, you will have to create a program that predicts the price of a car by using a linear function train with a gradient descent algorithm."
                 :isCover="true" />
 
-            <ProjectCard title="Hogwarts Magic Hat"
-                image="https://static.wikia.nocookie.net/harrypotter/images/6/60/McGonagall_and_the_Sorting_Hat-SS.png/"
-                description="Reconstruction of the Hogwarts Magic Hat! by applying Logistic regression to sort students based on ther abilites."
+            <ProjectCard title="DSLR" image="https://i.imgur.com/70PtVql.gif"
+                description="Data Science x Logistic Regression, is a project that aims to analyze a data set and predict Hogwarts House."
                 :isCover="true" />
 
             <ProjectCard title="N-Puzzle" image="/images/n-puzzle.png"
@@ -29,11 +30,16 @@
 
             <ProjectCard title="DeepEye"
                 image="https://raw.githubusercontent.com/aallali/DeepEye/main/Ressources/DeepEye-logo.png"
-                description="DeepEye a CLI that simply help you make search queries through files quickly using plain text or regex expressions, while having detailed output about it. The project meant to be simple and HELPFUL" />
+                description="a CLI that simply help you make search queries through files quickly using plain text or regex expressions, while having detailed output about it. The project meant to be simple and HELPFUL" />
 
+            <ProjectCard title="Inception of Things"
+                image="https://cloudvests.com/wp-content/uploads/2020/08/How-To-Create-a-Kubernetes-Cluster-Using-Kubeadm-on-Ubuntu-18.jpg"
+                description="This project aims to introduce you to kubernetes from a developer perspective. You will have to set up small clusters and discover the mechanics of continuous integration. At the end of this project you will be able to have a working cluster in docker and have a usable continuous integration for your applications Resources" :isCover="true" />
             <ProjectCard title="Red-Tetris"
-                image="https://raw.githubusercontent.com/aallali/RED-TETRIS/main/docs/playboard-Winner.png"
-                description="DeepEye a CLI that simply help you make search queries through files quickly using plain text or regex expressions, while having detailed output about it. The projeThe objective of this project is to develop a networked multiplayer tetrisgame from a stack of software exclusively Full Stack Javascript" />
+                image="https://img.joomcdn.net/42c6474dc9f0b60fb19e24871535d6bb96641832_original.jpeg"
+                description="The objective of this project is to develop a networked multiplayer tetris game from a stack of software exclusively Full Stack Javascript" :isCover="true" />
+
+
         </div>
     </div>
 </template>
