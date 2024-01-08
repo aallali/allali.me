a745a63d8b147c9df0f7e3c416bb86da91815d0e
3dida <44850191+aallali@users.noreply.github.com>
2024-01-08 17:24:03 +0100

    [+] : dynamic urls : @ProjectCard.vue

--- components/projects/ProjectCard.vue
+++ components/projects/ProjectCard.vue
@@ -1,8 +1,7 @@
 <template >
     <div>
         <div class="relative group overflow-hidden rounded-lg shadow-md dark:shadow-gray-300 dark:shadow-sm">
-            <img :class="isCover ? 'object-cover' : 'object-scale-down'"
-                class="h-60 w-full " :src="image" :alt="title" />
+            <img :class="isCover ? 'object-cover' : 'object-scale-down'" class="h-60 w-full " :src="image" :alt="title" />
 
             <div
                 class="flex flex-col opacity-0 group-hover:opacity-95 absolute inset-x-0 bottom-0 justify-center items-center text-md text-start bg-gray-800 text-white font-semibold h-full transition-all transform translate-y-32 group-hover:translate-y-0 duration-1000  rounded-lg p-4 ">
@@ -11,10 +10,8 @@
                     {{ description }}
                 </div>
                 <div class="m-auto flex gap-4 justify-center">
-                    <button
-                        :class="className.buttons">Demo</button>
-                    <button
-                    :class="className.buttons">Github</button>
+                    <button v-if="demo" :class="className.buttons"><nuxt-link :to="demo" target="_blank">Demo</nuxt-link></button>
+                    <button v-if="github" :class="className.buttons"><nuxt-link :to="github" target="_blank">Github</nuxt-link></button>
                 </div>
 
             </div>
@@ -28,5 +25,5 @@
 const className = {
     buttons: "mt-6 rounded-lg border-2 text-blue-300 border-blue-300 px-6 py-2.5 text-center text-xl font-medium capitalize leading-5  hover:bg-blue-500 hover:text-white hover:border-white focus:outline-none lg:mx-0 lg:w-auto"
 }
-defineProps<{ title: string, image: string, description: string, isCover?: boolean }>()
+defineProps<{ title: string, image: string, description: string, demo?: string, github?: string, isCover?: boolean }>()
 </script>
