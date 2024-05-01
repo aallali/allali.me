77111a52591b53b9541d77fccb154bd5bb96340c
3dida <44850191+aallali@users.noreply.github.com>
2024-05-01 16:08:33 +0100

    [update] : long bio

--- content/portfolio.data.ts
+++ content/portfolio.data.ts
@@ -6,10 +6,10 @@ export const infos = {
     welcomeHeadline: 'Hi 👋, I\'m Abdellah',
     briefBio: 'A <b class="text-blue-600">software engineer</b> and open-source enthusiast, I enjoy keeping this blog regularly updated with insights gained along my life journey and topics that pique my interest.',
     longBio:
-        `Hello, I'm Abdellah, a dedicated <b>software engineer</b> deeply passionate about the intricacies of <b>computer science</b>. My expertise spans modern <b>web technologies</b>, applied <b>machine learning</b>, and contributions to <b>open-source software</b>. Currently pursuing a <b>Master of Science in Computer Science</b>, primary focused around <b>database architecture</b> and <b>data</b>.
+        `Hello, I'm Abdellah, a dedicated <b>software engineer</b> deeply passionate database internals and how storage engines works. My expertise spans modern <b>web technologies</b>, applied <b>machine learning</b>, and contributions to <b>open-source software</b>. Just got my Computer Architect Degree, primary focused around <b>data</b> and <b>low level system programming</b>.
 <br>
 <br> 
-Throughout my journey, I've independently acquired proficiency in a diverse range of technologies. I document my learning experiences on this blog, covering everything from tech-savvy insights into specific tools to invaluable development patterns. Committed to staying abreast of the latest industry trends, I regularly update the blog with articles that delve into my interests and the insights gained from my day-to-day experiences. Furthermore, you can explore some of my noteworthy <b>side projects</b> on my public GitHub repository.`,
+Throughout my journey,as a self-taught developer, I've independently acquired proficiency in a diverse range of technologies. I document my learning experiences on this blog, covering everything from tech-savvy insights into specific tools to invaluable development patterns. I regularly update the blog with articles that delve into my interests and the insights gained from my day-to-day experiences. Furthermore, you can explore some of my noteworthy <b>side projects</b> on my public GitHub repository.`,
     portfolio: {
         subtitle: '',
         projects: [
